626b1fbc966ae3f1239fba258875e7e317a10550
natalia-batista <batistanatp@gmail.com>
2025-08-19 17:02:02 -0300

    Erro no construtor da airline

--- C14_AtividadeAeroportos/src/main/java/org/example/Airline.java
+++ C14_AtividadeAeroportos/src/main/java/org/example/Airline.java
@@ -3,4 +3,9 @@ package org.example;
 public class Airline {
     String name;
     String code;
+
+    public Airline(String name, int code) {
+        this.name = name;
+        this.code = code;
+    }
 }
