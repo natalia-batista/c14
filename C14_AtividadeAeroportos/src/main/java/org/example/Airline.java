3f4add2408d28da5afb7e7d75d85282883994e04
natalia-batista <batistanatp@gmail.com>
2025-09-01 12:49:06 -0300

    composicao airline flightinfo

--- C14_AtividadeAeroportos/src/main/java/org/example/Airline.java
+++ C14_AtividadeAeroportos/src/main/java/org/example/Airline.java
@@ -4,7 +4,7 @@ public class Airline {
     String name;
     String code;
 
-    public Airline(String name, int code) {
+    public Airline(String name, String code) {
         this.name = name;
         this.code = code;
     }
