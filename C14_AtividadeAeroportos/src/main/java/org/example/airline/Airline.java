80f58c873f5908fdc1804c66df9073bfdf949e48
natalia-batista <batistanatp@gmail.com>
2025-09-01 13:51:58 -0300

    nova classe passenger e funcionalidade de comprar ticket

--- C14_AtividadeAeroportos/src/main/java/org/example/airline/Airline.java
+++ C14_AtividadeAeroportos/src/main/java/org/example/airline/Airline.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.airline;
 
 public class Airline {
     String name;
---
+++ C14_AtividadeAeroportos/src/main/java/org/example/passengers/Passenger.java
@@ -0,0 +1,46 @@
+package org.example.passengers;
+
+import org.example.flight.FlightInfo;
+
+import java.util.Random;
+
+public class Passenger {
+    int passengerId;
+    String passengerName;
+    int passengerAge;
+    Random ticketNumber = new Random();
+    FlightInfo flightInfo;
+
+    public Passenger(int passengerId, String passengerName, int passengerAge, FlightInfo flightInfo) {
+        this.passengerId = passengerId;
+        this.passengerName = passengerName;
+        this.passengerAge = passengerAge;
+        this.flightInfo = flightInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPassenger [name=" + passengerName + ",\n id=" + passengerId + ",\n age="
+                + passengerAge;
+    }
+
+    public boolean buyTicket() {
+        boolean ticketAvailable = this.flightInfo.canBuyTicket();
+        if (ticketAvailable) {
+            int ticketNb = ticketNumber.nextInt((flightInfo.getCapacity() - 0) + 1);
+            this.printTicket(ticketNb);
+            return true;
+        }
+        return false;
+    }
+    
+    public void printTicket(int ticketNb) {
+        System.out.println("Ticket available");
+        System.out.println("---------------------------------------------------------");
+        System.out.println(this.flightInfo.toString());
+        System.out.println("---------------------------------------------------------");
+        System.out.println(this.toString());
+        System.out.println("Ticket number: " + ticketNb);
+    }
+
+}
