5cd8c1cd3481e8ef966a226452ca578610639943
natalia-batista <batistanatp@gmail.com>
2025-09-01 13:28:02 -0300

    adicao de metodos em flightinfo e airport

--- C14_AtividadeAeroportos/src/main/java/org/example/FlightInfo.java
+++ C14_AtividadeAeroportos/src/main/java/org/example/FlightInfo.java
@@ -3,12 +3,12 @@ package org.example;
 public class FlightInfo {
     private int flightId;
     private String flightName;
-    private String capacity;
+    private int capacity;
     private double duration;
     private double price;
     private double distance;
 
-    public FlightInfo(int flightId, String flightName, String capacity, double duration, double price, double distance,
+    public FlightInfo(int flightId, String flightName, int capacity, double duration, double price, double distance,
                       String airlineName, String airlineCode) {
         this.flightId = flightId;
         this.flightName = flightName;
@@ -28,6 +28,24 @@ public class FlightInfo {
     public String toString() {
         return "Flight Info [flightId=" + flightId + ",\n flightName=" + flightName +
                 ",\n capacity=" + capacity + ",\n duration=" + duration + ",\n price=" + price + ",\n distance=" + distance + "]";
+    }
+
+    public double pricePerKm() {
+        if (this.distance > 0){
+            return this.price / this.distance;
+        }
+        return 0;
+    }
+
+    public double averageSpeed() {
+        if (this.duration > 0){
+            return this.distance / this.duration;
+        }
+        return 0;
+    }
+
+    public boolean isCheaperThan(FlightInfo other) {
+        return this.price < other.price;
     }
 
 }
